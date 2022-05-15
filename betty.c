2dbfa6421f9cdbea24a58c922bf04f7fc2aea842
captjay98 <captjay98@gmail.com>
2022-05-15 22:50:37 +0100

    A code that satisfies betty

---
+++ betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints to stdout
+* Description: trying to impress betty
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	puts("Trying to impress betty");
+
+	return (0);
+}
